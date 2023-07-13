ba551989fd05cb004dbf075453590a0edbfaeca4
Jose Geraldo <jgsouzadevweb@gmail.com>
2023-07-13 13:54:26 -0300

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script>
+import Github from './components/Github.vue'
+export default {
+components:{
+  Github
+}
+}
+
+</script>
+
+<template>
+  <Github/>
+</template>
+
+<style>
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body{
+  background: #1c1a1d;
+  color: #e5e5e5;
+}
+#app{
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  min-height: 100vh;
+  max-width: 40rem;
+  margin: 0 auto;
+  text-align: center;
+}
+img{
+  display: block;
+  border-radius: 999px;
+  border: 1px solid #e5e5e5;
+  margin: 1rem auto;
+  width: 8rem;
+  height: 8rem;
+}
+h1, h2{
+  color: #f64348;
+  margin: 1rem auto .25rem;  
+}
+h3{
+  margin: 1rem auto .25rem;
+}
+span{
+  display: block;
+  margin: 1rem auto;  
+}
+input, button{
+  max-width: 20rem;
+  padding: .5rem;
+}
+input{
+  background: #1c1a1d;
+  border: 1px solid #f64348;
+  border-radius: .25rem;
+  color: #e5e5e5;
+  margin: 1rem 1rem 1rem 0;
+}
+button{
+  background: #f64348;
+  border: unset;
+  border-radius: .25rem;
+  color: #1c1a1d;
+  font-size: 1rem;
+  font-weight: 700;
+  text-transform: uppercase;
+}
+button:hover{
+  cursor: pointer;
+  filter: brightness(.95);
+}
+a{
+  color: #f64348;
+}
+
+</style>
---
+++ src/components/Github.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data(){
+    return{
+      name: 'JG Souza Jr Dev',
+      login: '',
+      bio: '',
+      company:'',
+      avatar_url:'https://unsplash.it/256',
+      searchInput: ''
+    }
+  },
+  methods:{
+    async fetchGithubUser(){
+      const res = await fetch(`https://api.github.com/users/${this.searchInput}`);
+      const {login, name, bio, company, avatar_url} = await res.json()
+
+      this.login = login
+      this.name = name
+      this.bio = bio
+      this.company = company
+      this.avatar_url = avatar_url
+   }
+  }
+}
+</script>
+
+<template>    
+      <main>
+        <h1>GitHub User Data</h1>
+      <input type="text" v-model="searchInput">
+      <button @click="fetchGithubUser">Load User</button>
+      <img :src="avatar_url">
+      <strong>@{{login}}</strong>
+      <h2>{{name}}</h2>
+      <h3>{{company}}</h3>
+      <span>{{bio}}</span>
+      </main>
+</template>
