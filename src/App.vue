a238b2794a23df045344fdaff29e0570949b6d73
Jose Geraldo <jgsouzadevweb@gmail.com>
2023-07-24 10:07:44 -0300

    Adicionando Style

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup>
+
 import {ref} from 'vue';
-import CompositonApi from './components/CompositonApi.vue';
+import Profile from './components/Profile.vue';
+const isOpen = ref(true)
+
+</script>
+
+<!--
 // import OptionsApi from './components/OptionsApi.vue'
 // import MetodoSetup from './components/MetodoSetup.vue';
 // export default {
@@ -9,14 +15,12 @@ import CompositonApi from './components/CompositonApi.vue';
 //   CompositonApi,
 //   MetodoSetup
 // }
-// }
-const isOpen = ref(true)
-</script>
+// } -->
 
 <template>
  <div>
   <input type="checkbox" v-model="isOpen" > {{ isOpen }}
-   <CompositonApi v-if="isOpen"/>
+   <Profile v-if="isOpen"/>
   <!-- <OptionsApi/> -->
   <!-- <MetodoSetup/> -->
  </div>
@@ -39,49 +43,7 @@ body{
   margin: 0 auto;
   text-align: center;
 }
-img{
-  display: block;
-  border-radius: 999px;
-  border: 1px solid #e5e5e5;
-  margin: 1rem auto;
-  width: 8rem;
-  height: 8rem;
-}
-h1, h2{
-  color: #f64348;
-  margin: 1rem auto .25rem;  
-}
-h3{
-  margin: 1rem auto .25rem;
-}
-span{
-  display: block;
-  margin: 1rem auto;  
-}
-input, button{
-  max-width: 20rem;
-  padding: .5rem;
-}
-input{
-  background: #1c1a1d;
-  border: 1px solid #f64348;
-  border-radius: .25rem;
-  color: #e5e5e5;
-  margin: 1rem 1rem 1rem 0;
-}
-button{
-  background: #f64348;
-  border: unset;
-  border-radius: .25rem;
-  color: #1c1a1d;
-  font-size: 1rem;
-  font-weight: 700;
-  text-transform: uppercase;
-}
-button:hover{
-  cursor: pointer;
-  filter: brightness(.95);
-}
+
 a{
   color: #f64348;
 }
